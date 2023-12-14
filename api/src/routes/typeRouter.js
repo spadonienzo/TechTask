b2c94297c37a720f1a43ddee13a60cffade67d3e
spadonienzo <spadonienzo@gmail.com>
2023-12-14 15:29:49 -0300

    ...

--- api/src/routes/typeRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const typeRouter = Router()
-
-const typeHandler = require('../handlers/typeHandler')
-
-typeRouter.get('/', typeHandler)
-
-module.exports = typeRouter
